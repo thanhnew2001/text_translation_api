6f3aa83fa7681833dea180bd70b0a26cfef2ef2f
thanhnew2001 <69960815+thanhnew2001@users.noreply.github.com>
2024-06-20 12:09:17 +1000

    Update api.py

--- api.py
+++ api.py
@@ -32,24 +32,24 @@ ALLOWED_EXTENSIONS = {'txt'}
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# def upload_file_to_s3(file_path, bucket_name, object_name):
-#     try:
-#         print(file_path)
-#         s3.upload_file(file_path, bucket_name, object_name)
-#         return True
-#     except Exception as e:
-#         print(f"Error uploading file to S3: {e}")
-#         return False
-
 def upload_file_to_s3(file_path, bucket_name, object_name):
     try:
         print(file_path)
-        with open(file_path, 'rb') as file:
-            s3.upload_fileobj(file, bucket_name, object_name)
+        s3.upload_file(file_path, bucket_name, object_name)
         return True
     except Exception as e:
         print(f"Error uploading file to S3: {e}")
         return False
+
+# def upload_file_to_s3(file_path, bucket_name, object_name):
+#     try:
+#         print(file_path)
+#         with open(file_path, 'rb') as file:
+#             s3.upload_fileobj(file, bucket_name, object_name)
+#         return True
+#     except Exception as e:
+#         print(f"Error uploading file to S3: {e}")
+#         return False
 
 
 @app.route('/')
